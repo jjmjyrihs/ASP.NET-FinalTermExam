598d39a68bd2d024f1bf896dd9bd16aa8179d9ef
LG <1104137128@gm.kuas.edu.tw>
2017-06-23 13:46:43 +0800

    Start

---
+++ ASP.MVC/ASP.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.MVC.Startup))]
+namespace ASP.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
